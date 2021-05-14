d7b11b43efaddfb47a1a36922508686bfb8cb5ed
Spencer Gunning <31055834+aemx@users.noreply.github.com>
2021-05-14 10:21:17 -0400

    April 12 work

--- src/functions/git.js
+++ src/functions/git.js
@@ -1,10 +1,14 @@
 import * as fx from '../functions/fx';
 const fs = require('fs');
 
-const rev = fs.readFileSync('.git/refs/heads/main', 'utf-8').slice(0,7)
+const rev = fs.readFileSync('.git/refs/heads/main', 'utf-8').slice(0,2)
 
 export const version = () => {
     if (fx.checkClass('version')) {
-        fx.setClass('version', `rev. ${new Date().getFullYear()}-${rev} · <a href="./site-map"> View Site Map</a>`);
+        const y = (new Date().getFullYear() - 2020).toString(36) ;
+        const m = (new Date().getMonth()).toString(36);
+        const d = (new Date().getDate()).toString(36);
+        const revF = `${y}${m}${d}${rev}`.toUpperCase()
+        fx.setClass('version', `Revision ${revF} · <a href="./site-map"> View Site Map</a>`);
     }
 }
--- src/js/main.js
+++ src/js/main.js
@@ -226,3 +226,9 @@ window.onload = function() {
     pageInjector(page);
     Git.version();
 };
+
+if (!url.includes('.html') && !url.includes('#') && url !== 'http://localhost:1234/') {
+    window.location.href = url + '.html'
+} else if (!url.includes('.html') && url.includes('#')) {
+    window.location.href = url.split("#")[0] + '.html#' + url.split("#")[1]
+}
