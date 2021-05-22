8ec309942ccd0ab25b0e0233b63df1cf59fbf020
Spencer Gunning <31055834+aemx@users.noreply.github.com>
2021-05-22 17:52:51 -0400

    Actually actually the final fix

--- src/js/main.js
+++ src/js/main.js
@@ -256,9 +256,3 @@ window.onload = function() {
     pageInjector(page);
     Data.rev();
 };
-
-if (!url.includes('.html') && !url.includes('#') && url !== 'http://localhost:1234/') {
-    window.location.href = url + '.html'
-} else if (!url.includes('.html') && url.includes('#')) {
-    window.location.href = url.split("#")[0] + '.html#' + url.split("#")[1]
-}
