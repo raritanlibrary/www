106be0c1d7be6db234bb9e18138443c570097e2b
Spencer Gunning <31055834+aemx@users.noreply.github.com>
2021-05-22 17:50:33 -0400

    Deploy fix should be final

--- src/functions/data.js
+++ src/functions/data.js
@@ -160,3 +160,11 @@ export const eventInjector = () => {
         fx.setClass(`calendar-events`, eventList)
     }
 }
+
+// Git data
+let tag = fs.readFileSync('src/data/_REV', 'utf8');
+export const rev = () => {
+    if (fx.checkClass('version')) {
+        fx.setClass('version', `Revision ${tag} · <a href="./site-map"> View Site Map</a>`);
+    }
+}
