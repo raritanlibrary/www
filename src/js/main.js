7d698a3d5c1d9e83ef85649e630f2cb7b04e87a7
Spencer Gunning <31055834+aemx@users.noreply.github.com>
2021-06-01 02:55:53 -0400

    Final draft

--- src/js/main.js
+++ src/js/main.js
@@ -252,19 +252,17 @@ Nav.dropdown();
 Nav.sticky();
 Access.preload();
 
+
+
 window.onload = function() {
     Time.injector();
-    Data.eventInjector();
+    if (!page.includes('programs')) {
+        Data.eventInjector();
+    };
     pageInjector(page);
     Access.hicontrast();
     Data.rev();
 };
-
-if (!url.includes('.html') && !url.includes('#') && url !== 'http://localhost:1234/') {
-    window.location.href = url + '.html'
-} else if (!url.includes('.html') && url.includes('#')) {
-    window.location.href = url.split("#")[0] + '.html#' + url.split("#")[1]
-}
 
 if (!url.includes('.html') && !url.includes('#') && url !== 'http://localhost:1234/') {
     window.location.href = url + '.html'
