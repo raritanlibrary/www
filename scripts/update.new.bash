b4abda8cceff77f8ca558aee8918606e0aff61a3
Spencer Gunning <swcgunning@gmail.com>
2021-08-06 14:47:36 -0400

    Aug. 6

---
+++ scripts/update.new.bash
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+if [ -f .env ]
+then
+    export $(cat new.env | xargs)
+fi
+
+"C:\Program Files\7-Zip\7z.exe" a www.zip "dist\\"
+"C:\Program Files\7-Zip\7z.exe" rn www.zip dist www
+"C:\Program Files (x86)\WinSCP\WinSCP.com" << EOF
+open ssh://$u:@$h -privatekey=$k
+cd Users\Administrator.RPL\Downloads
+put www.zip
+rmdir www
+call cd C:/Users/Administrator.RPL/Downloads && unzip www.zip
+rm www.zip
+EOF
+rm www.zip
