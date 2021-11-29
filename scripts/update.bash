1ac80efbbe1b09ee9da72882d75f6d2f419beb79
Spencer Gunning <swcgunning@gmail.com>
2021-11-29 04:00:28 -0500

    Final push for new host

--- scripts/update.bash
+++ scripts/update.bash
@@ -2,16 +2,14 @@
 
 if [ -f .env ]
 then
-    export $(cat new.env | xargs)
+    export $(cat .env | xargs)
 fi
 
 "C:\Program Files\7-Zip\7z.exe" a www.zip "dist\\"
 "C:\Program Files\7-Zip\7z.exe" rn www.zip dist www
 "C:\Program Files (x86)\WinSCP\WinSCP.com" << EOF
 open ssh://$u:@$h -privatekey=$k
-put www.zip
+put -delete www.zip 
 rmdir www
-call cd C:/Users/Administrator.RPL/Downloads && unzip www.zip
-rm www.zip
+call cd C && unzip www.zip && rm www.zip
 EOF
-rm www.zip
