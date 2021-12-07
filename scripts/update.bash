d1a146d8cedd33a8ff01091866ca58f9e342f645
Spencer Gunning <swcgunning@gmail.com>
2021-12-07 15:05:07 -0500

    Add December newsletters, adjust updater

--- scripts/update.bash
+++ scripts/update.bash
@@ -9,8 +9,8 @@ fi
 "C:\Program Files\7-Zip\7z.exe" rn www.zip dist www
 "C:\Program Files (x86)\WinSCP\WinSCP.com" << EOF
 open ssh://$u:@$h -privatekey=$k
-cd /
 put -delete www.zip 
 rmdir www
-call unzip www.zip && rm www.zip
+call unzip -o www.zip "!"
+rm www.zip
 EOF
