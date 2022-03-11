11ec46a29c008cf35eff53c2cd3a566aaee6069a
Spencer Gunning <swcgunning@gmail.com>
2022-03-11 11:27:02 -0500

    Fix gen script issue with creating static files
    Resolves #13

--- scripts/generate.sh
+++ scripts/generate.sh
@@ -7,11 +7,12 @@ rev=$(git rev-list --count main)
 printf '%04d' $rev > src/data/_REV
 
 # Make necessary folders
-mkdir "dist/img"
+[[ ! -d "dist/docs" ]] && mkdir "dist/docs"
+[[ ! -d "dist/img" ]] && mkdir "dist/img"
 
 # Copy static files
-for static in "docs" "img/events" "img/kids" "img/news" "img/promo" "robots.txt" ".htaccess"; do
-    cp -R "src/${static}" "dist/${static}"
+for static in "docs/events" "img/events" "img/kids" "img/news" "img/promo" "robots.txt" ".htaccess"; do
+    [[ ! -d "dist/${static}" ]] && cp -R "src/${static}" "dist/${static}"
 done
 
 echo "Finished generation."
