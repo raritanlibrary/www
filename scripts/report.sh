2186f6de74604b5b54d32b2ede71912870e5d2e0
Spencer Gunning <swcgunning@gmail.com>
2022-03-15 15:09:56 -0400

    Adjust Lighthouse reporting script to account for Desktop devices

--- scripts/report.sh
+++ scripts/report.sh
@@ -3,6 +3,8 @@
 # Calculate timestamp and make a directory with it
 timestamp=$(date +%y%m%d)
 mkdir reports/$timestamp
+mkdir reports/$timestamp/mobile
+mkdir reports/$timestamp/desktop
 
 # Generate report for each .pug file
 for file in ./src/*; do
@@ -10,11 +12,13 @@ for file in ./src/*; do
         file=$(basename -- "$file")
         fname="${file%.*}"
         if [[ $fname == "index" ]]; then
-           echo "Generating report for index.html..."
-           lighthouse "https://raritanlibrary.org" --quiet=true --chrome-flags="--headless" --output=html --output-path=reports/$timestamp/$fname.html
+           echo "Generating reports for index.html..."
+           lighthouse "https://raritanlibrary.org" --quiet=true --chrome-flags="--headless" --output=html --preset=desktop --output-path=reports/$timestamp/desktop/$fname.html
+           lighthouse "https://raritanlibrary.org" --quiet=true --chrome-flags="--headless" --output=html --output-path=reports/$timestamp/mobile/$fname.html
         elif [[ $fname != "$" ]] && [[ $fname != "robots" ]]; then
-           echo "Generating report for ${fname}.html..."
-           lighthouse "https://raritanlibrary.org/${fname}" --quiet=true --chrome-flags="--headless" --output=html --output-path=reports/$timestamp/$fname.html
+           echo "Generating reports for ${fname}.html..."
+           lighthouse "https://raritanlibrary.org/${fname}" --quiet=true --chrome-flags="--headless" --output=html --preset=desktop --output-path=reports/$timestamp/desktop/$fname.html
+           lighthouse "https://raritanlibrary.org/${fname}" --quiet=true --chrome-flags="--headless" --output=html --output-path=reports/$timestamp/mobile/$fname.html
         fi
     fi
 done
