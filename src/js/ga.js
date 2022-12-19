7257cf57d65bab78b5345a814eb4e6ac40678bd4
Spencer Gunning <swcgunning@gmail.com>
2022-12-19 13:40:23 -0500

    Add proper tracking for some eResources

--- src/js/ga.js
+++ src/js/ga.js
@@ -1,3 +1,8 @@
+(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-ND6P9J5');
 window.ga=window.ga||function(){(ga.q=ga.q||[]).push(arguments)};ga.l=+new Date;
 ga('create', 'UA-130594894-1', 'auto');
 ga('send', 'pageview');
