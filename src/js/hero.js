72e3258216350f4d02ade99074d33b96026ac37d
Spencer Gunning <swcgunning@gmail.com>
2023-08-04 16:58:01 -0400

    August 04 work

--- src/js/hero.js
+++ src/js/hero.js
@@ -2,31 +2,36 @@ import * as util from './util';
 
 // Slide information
 let curSlide = 0;
-const slides = document.querySelectorAll(".hero-slide");
-const maxSlide = slides.length;
-const slideNum = n => n % maxSlide;
+const mod = (n, m) => ((n % m) + m) % m;
 
 // Navigation click listener
 util.addClickListener(e => {
-    const previous = document.querySelector(".hero-nav-prev");
-    const next = document.querySelector(".hero-nav-next");
-    if (previous.contains(e.target)) {
-        curSlide = curSlide === 0 ? maxSlide - 1 : curSlide - 1;
-        slides.forEach(slide => slide.style.transform = `translateX(${100 * - curSlide}%)`);
-    } else if (next.contains(e.target)) {
-        curSlide = curSlide === maxSlide - 1 ? 0 : curSlide + 1;
-        slides.forEach(slide => slide.style.transform = `translateX(${100 * - curSlide}%)`);
+    const slideContainer = document.getElementById('hero-content');
+    let slides = Array.from(slideContainer.children);
+    const maxSlide = slides.length;
+    const previous = document.getElementById("hero-prev");
+    const next = document.getElementById("hero-next");
+    if (previous.contains(e.target) || next.contains(e.target)) {
+        // Unset slide dot
+        document.getElementById(`hero-dot-${curSlide}`).classList.remove('hero-select-current');
+
+        // Go back/forward 1 slide
+        curSlide = previous.contains(e.target) ? mod(curSlide - 1, maxSlide) : mod(curSlide + 1, maxSlide);
+
+        // Shift slide
+        slides.forEach(slide => slide.classList.add('shift'));
+        
+        // Rearrange slide order
+        previous.contains(e.target) ? slides.unshift(slides.pop()) : slides.push(slides.shift());
+        slideContainer.innerHTML = slides.map(x => x.outerHTML).join('');
+        
+        // Shift slide back
+        slides = Array.from(slideContainer.children);
+        slides.forEach(slide => slide.classList.add('shift-transition'));
+        setTimeout(() => slides.forEach(slide => slide.classList.remove('shift')), 1);
+        setTimeout(() => slides.forEach(slide => slide.classList.remove('shift-transition')), 60);
+
+        // Set slide dot
+        document.getElementById(`hero-dot-${curSlide}`).classList.add('hero-select-current');
     }
 });
-
-/*
-const moveChoiceTo = (elem_choice, direction) => {
-    var span = elem_choice.parentNode,
-        td = span.parentNode;
-    if (direction === -1 && span.previousElementSibling) {
-        td.insertBefore(span, span.previousElementSibling);
-    } else if (direction === 1 && span.nextElementSibling) {
-        td.insertBefore(span, span.nextElementSibling.nextElementSibling)
-    }
-}
-*/
